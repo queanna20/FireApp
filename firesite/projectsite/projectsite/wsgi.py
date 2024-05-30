2424f7167b93280957420130424114ea8290db1f
Queanna Brittany <202080061@psu.palawan.edu.ph.com>
2024-05-30 16:18:43 +0800

    Modify

---
+++ firesite/projectsite/projectsite/wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+#path where you can find the mange.py
+
+path = '/home/superjubnick/FireApp/boilerplate/projecsite'
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectsite.settings")
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
